8c32a03ae07f020f1261cc5ff24aed9ab03c3d8d
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-09-22 22:48:18 +0800

    Help and Contact.

--- app/src/main/java/com/example/musicloud/fragment/HelpFragment.kt
+++ app/src/main/java/com/example/musicloud/fragment/HelpFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.musicloud.R
 import com.example.musicloud.ViewModelFactories.HelpViewModelFactory
 import com.example.musicloud.adapters.HelpAdapter
 import com.example.musicloud.databinding.HelpFragmentBinding
@@ -44,9 +46,27 @@ class HelpFragment: Fragment() {
             layoutManager = LinearLayoutManager (requireActivity())
         }
 
+        binding.constactButton.setOnClickListener {
+            showContactDialog()
+        }
+
         return binding.root
     }
 
+
+    private fun showContactDialog () {
+
+        val alertDialog = AlertDialog.Builder (requireActivity())
+        alertDialog.setTitle (R.string.contactInfo)
+            .setMessage (R.string.contactDescription)
+            .setNeutralButton (R.string.ok) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+
+
+        alertDialog.create()
+        alertDialog.show()
+    }
 
     override fun onDestroyView() {
         _binding = null
